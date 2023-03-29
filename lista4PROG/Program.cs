75e66710a32b8542e7ab47f90c3f0fcdab171cb1
LuizRicardo777 <luizricaropereirasalles@gmail.com>
2023-03-29 12:48:16 -0300

    Adicionar arquivos de projeto.

---
+++ lista4PROG/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista4PROG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1- é falsa, pois uma variavel int so recebe numeros inteiros e não consegue trabalhar com números depois da vírgula.
+            // int v;
+            //v = float.Parse(Console.ReadLine());
+            // ja no contrario pode ser possivel jogar um float dentro de um int
+            // float v;
+            //   v = int.Parse(Console.ReadLine());
+
+
+            //-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+            //2-o console.read.line() retorna o valor da linha interia que o usuario passa, ja o Console. Read() Retorna o valor em ASCII do primeiro caractere digitado na linha, ele lê e faz a conversão.
+
+
+            //----------------------------------------------------------------------------------------------------------------------------------------------
+
+            //3-tem que tomar cuidado com o tipo de exercico que é pedido, pois essas variaveis sao apenas para numeros e tomar cuidado com o tipo de numero que elas recebem
+
+            //-------------------------------------------------------------------------------------------------------------------------------------------------
+
+            //4- numero 8 aparece mensagem 2, numero 27 aparece mensagem 2 e por ultimo o numero 28 aparece mensagem 1.
+            //int num, num2, num3;
+            //Console.WriteLine("entre com algum numero : ");
+            //num = int.Parse(Console.ReadLine());
+            //num2 = num / 2;
+
+            //num3 = num % 2;
+            //if ( num3 == 0 && num2*2 > 10)
+            // Console.WriteLine("mensagem 1");
+
+            //  else
+            //    Console.WriteLine("mensagem 2");
+
+            //Console.ReadKey();
+            //-------------------------------------------------------------------------------
+
+            //float s, so, v, t;
+            //Console.WriteLine("qual a velocidade?");
+            //v = float.Parse(Console.ReadLine());
+            //Console.WriteLine("qual a posição inicial ?");
+            //so = float.Parse(Console.ReadLine());
+            //Console.WriteLine("qual o instante?");
+            //t = float.Parse(Console.ReadLine());
+
+            //s = so + v * t;
+            //Console.WriteLine("a posição final é " + s);
+            //Console.ReadKey();
+            //5-exercico 
+
+            //--------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+        }
+    }
+}
